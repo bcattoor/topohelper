053642b88ef140eb4f5fc3246e1363e075a876f4
bcattoor <bjorn.cattoor@infrabel.be>
2021-03-11 10:53:48 +0100

    Update some packeges

--- TopoHelper/Properties/AssemblyInfo.cs
+++ TopoHelper/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision
 // Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.07.203.28")]
-[assembly: AssemblyFileVersion("2020.07.147.28")]
+[assembly: AssemblyVersion("2021.03.250.11")]
+[assembly: AssemblyFileVersion("2021.03.191.11")]

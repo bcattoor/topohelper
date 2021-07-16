0cf483f00c30615bbeb3895fbd8914cb0e739404
generic-user <orhiannono@gmail.com>
2021-07-16 15:55:32 +0200

    Update StringExtensions.cs
    Fixed issue with double spaces.

--- TopoHelper/Model/String/StringExtensions.cs
+++ TopoHelper/Model/String/StringExtensions.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TopoHelper.Model.String
 {
     internal static class StringExtensions
     {
+        public static string CapitalizeFirstLetter(this string s)
+        {
+            return char.ToUpper(s.First()) + s.Substring(1).ToLower();
+        }
+
         public static string UnPascalCase(this string text)
         {
             if (string.IsNullOrWhiteSpace(text))
@@ -28,11 +34,16 @@ namespace TopoHelper.Model.String
             return newText.ToString();
         }
 
-        public static string ToFriendlyCommandName(this string s) => s.Replace("IAMTopo_", "").Trim()
+        public static string ToFriendlyCommandName(this string s) =>
+            Regex.Replace(
+             s.Replace("IAMTopo_", "")
+            .Trim()
             .Replace("2D", "Tweeedeee").Replace("3D", "Treeedeee")
             .Replace("2", "Two")
-            .UnPascalCase()
-            .Replace("Treeedeee", "3D").Replace("Tweeedeee", "2D")
-            .Replace("_", " ");
+            .UnPascalCase().CapitalizeFirstLetter()
+            .Replace("Treeedeee", "3D").Replace("Tweeedeee", "2D") //Lower and upercase
+            .Replace("treeedeee", "3D").Replace("tweeedeee", "2D") //Lower and upercase
+            .Replace("_", " "),
+             @"\s+", " "); //remove double spaces
     }
 }

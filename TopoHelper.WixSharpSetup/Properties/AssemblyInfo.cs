c1e7a984c0e42d576b03efd196855ebd35ac3b10
generic-user <orhiannono@gmail.com>
2021-07-26 23:51:16 +0200

    Update AssemblyInfo.cs
    AssemblyTrademark

--- TopoHelper.WixSharpSetup/Properties/AssemblyInfo.cs
+++ TopoHelper.WixSharpSetup/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Infrabel")]
 [assembly: AssemblyProduct("TopoHelper")]
 [assembly: AssemblyCopyright("Copyright © Infrabel")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("© Infrabel")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible to
